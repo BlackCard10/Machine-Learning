361ea83074c6b1b6b32db936e6cb640bbf86b58b
Blackcard10 <BlackCard10@users.noreply.github.com>
2016-02-02 11:40:29 -0800

    Preprocessing Data and Final Solution

--- Testing Kaggle.R
+++ Testing Kaggle.R
@@ -1,5 +1,6 @@
 
 # Open the packages necessary
+
 library(rpart)
 library(rattle)
 library(rpart.plot)
@@ -22,16 +23,8 @@ fancyRpartPlot(my_tree)
 # Use the printcp function to print out the error under varrying levels of the complexity parameter 
 cp <- printcp(my_tree)
 
-# Predict the outcome of the model using the prediction matrix
-pred <- predict(my_tree, train, type = "class")
 
-# Create a confusion matrix 
-conf <- table(train$QuoteConversion_Flag, pred)
-
-# Test the accuracy
-acc <- sum(diag(conf))/sum(conf)
-
-# Clone the train data set to use for feature induction
+# Clone the train data set to use for feature testing
 train.FI <- train
 
 
@@ -50,35 +43,100 @@ blanks_matrix <- rbind(names_na, number_na)
 blanks_matrix
 
 
-# Transform NAs in train.FI PropertyField29 to 0's.  O's are choose as this 
+# Transform NAs in train.FI PropertyField29 to 0's.  O's are choosen as this 
 # is the mode of the feature column
 na.indexPF29 <- which(is.na(train.FI$PropertyField29))
-train.FI$PropertyField29[na.indexPF29] <- -1
+train.FI$PropertyField29[na.indexPF29] <- 0
 
 # Transform NAs in train.FI in PersonalField84 to 0's. 0's are chosen as this
 # is the mode of the feature column
 na.indexPF84 <- which(is.na(train.FI$PersonalField84))
-train.FI$PersonalField84[na.indexPF84] <- -1
+train.FI$PersonalField84[na.indexPF84] <- 0
 
 # Use train.FI in a new decision tree model
 my.treeFI <- rpart(QuoteConversion_Flag ~ ., train.FI, method = "class")
 
-# Predict the outcome of the model using the prediction 
-predFI <- predict(my.treeFI, train.FI, type = "class")
-
-# Create a confusion matrix 
-confFI <- table(train.FI$QuoteConversion_Flag, pred)
-
-# Test the accuracy
-accFI <- sum(diag(confFI))/sum(confFI)
 
 # Look at the cp matrix to determine the 10 parts cross validation 
 cpFI <- printcp(my.treeFI)
 
 
 
+# Find out which factor levels are unequal.  
+
+factorCheck <- function(x,y) {
+
+	for(attr in colnames(x)) {
+
+    	if (is.factor(x[[attr]])) {
+    
+       	 new.levels <- setdiff(levels(x[[attr]]), levels(y[[attr]]))
+        
+        	if ( length(new.levels) == 0 ){ 
+        		print(paste(attr, '- no new levels')) 
+        
+         	} else {
+         
+            	print(c(paste(attr, length(new.levels), 'of new levels, e.g.'), head(new.levels, 2)))
+        
+        	}
+    	}
+	}
+}
+
+# Replace unequal value types between test and train.   
+
+test$PersonalField16[which(test$PersonalField16 == "XG")] <- NA 
+test$PersonalField16[which(test$PersonalField16 == "YG")] <- NA 
+test$PersonalField16[which(test$PersonalField16 == "ZM")] <- NA 
+                           
+test$PersonalField17[which(test$PersonalField17 == "XF")] <- NA
+test$PersonalField17[which(test$PersonalField17 == "XZ")] <- NA
+test$PersonalField17[which(test$PersonalField17 == "YO")] <- NA
+test$PersonalField17[which(test$PersonalField17 == "ZJ")] <- NA
+
+test$PersonalField18[which(test$PersonalField18 == "XB")] <- NA
+
+test$PersonalField19[which(test$PersonalField19 == "ZS")] <- NA
 
 
+test$PropertyField5[which(test$PropertyField5 == "")] <- "Y"  # This is the mode of PF5
+
+
+test$PropertyField7[which(test$PropertyField7 == "T")] <- NA
+
+
+test$PropertyField30[which(test$PropertyField30 == "")] <- "N" # This is the mode of PF30
+
+
+test$PropertyField37[which(test$PropertyField37 == " ")] <- "N" # This is the mode of PF37
+
+
+
+# Find the optimal 'cp' from print.cp using the best version of tree.  
+Optimal_CP <- my_tree$cptable[which.min(my_tree$cptable[,"xerror"])]
+
+# View the CP with the lowest relative error 
+plotcp(my_tree)
+
+
+# Prune the tree using the optimal CP
+my_treePruned <- prune(my_tree, cp = my_tree$cptable[which.min(my_tree$cptable[,"xerror"])])
+
+# Plot pruned tree. Notice that this is the tree given initially before pruning.  
+fancyRpartPlot(my_treePruned)
+
+# Use the model to Predict the test data
+Prediction <-predict(my_tree, test, type = "class")
+
+# Form a submission dataframe for Kaggle
+Submission <- data.frame(QuoteNumber = test$QuoteNumber, QuoteConversion_Flag = Prediction)s
+
+# Write a csv that contains all submission
+write.csv(Submission, file = "firstSubmission.csv", colnames = TRUE) 
+
+
+ 
 
 
 
